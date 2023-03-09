cbdad25162dd8dca5d8e49c8e35c1701ac37556e
golphat1 <golphat@yahoo.com>
2023-03-09 09:35:34 -0800

    tasks

--- 0x02-python-import_modules/add_0.py
+++ 0x02-python-import_modules/add_0.py
@@ -1,10 +1,14 @@
 #!/usr/bin/python3
+
 def add(a, b):
-    """My addition function
+    """My addition function.
+
     Args:
-    a: first integer
-    b: second integer
+        a: first integer.
+        b: second integer.
+
     Returns:
-    The return value. a + b
+        The return value. a + b
     """
-    return (a + b)
+    return a + b
+
