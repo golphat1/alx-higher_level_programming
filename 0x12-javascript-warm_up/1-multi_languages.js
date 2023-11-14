f7e7b0b89c934117b5e44c038ce5e8a8345e2e50
golphat1 <golphat@yahoo.com>
2023-11-14 04:25:35 -0800

    task1

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,12 +1,4 @@
 #!/usr/bin/node
-
-/* script that prints 3 lines
- * "C is fun"
- * Python is cool"
- * “JavaScript is amazing”
- */
-
-// declaring constants
 const msg1 = "C is fun";
 const msg2 = "Python is cool";
 const msg3 = "JavaScript is amazing";
