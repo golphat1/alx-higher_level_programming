b1cf83cc37a976429a08b123e88c4e42abc43cd4
golphat1 <golphat@yahoo.com>
2023-11-15 08:24:15 -0800

    task1

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-const msg1 = "C is fun";
-const msg2 = "Python is cool";
-const msg3 = "JavaScript is amazing";
-console.log(msg1);
-console.log(msg2);
-console.log(msg3);
+const firstmsg = 'C is fun';
+const secondmsg = 'Python is cool';
+const thirdmsg = 'JavaScript is amazing';
+console.log(firstmsg);
+console.log(secondmsg);
+console.log(thirdmsg);
